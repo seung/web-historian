ec3979cd3aa4b08aae0bc2adafcd5c2d9d7538ce
Hack Reactor Students <accounts@hackreactor.com>
2013-04-03 16:37:22 -0700

    pass all the tests

--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,4 +1,8 @@
 var fs = require("fs");
+var http = require('http-get');
+var dataDir = process.env.HOME + "/Desktop/tony/web-historian/data/sites.txt";
+var siteDir = process.env.HOME + "/Desktop/tony/web-historian/testdata/sites/";
+var testDataDir = process.env.HOME + "/Desktop/tony/web-historian/spec/testdata/sites/";
 
 exports.readUrls = function(filePath, cb){
   var content = fs.readFileSync(filePath, 'utf8');
@@ -7,5 +11,17 @@ exports.readUrls = function(filePath, cb){
 }
 
 exports.downloadUrls = function(urls){
-  // fixme
+  exports.readUrls(dataDir, function(urls) {
+
+    for(var i = 0; i < urls.length; i++) {
+      http.get(urls[i], testDataDir + urls[i], function (error, result) {
+        if (error) {
+          console.error('error : ' + error);
+        } else {
+          console.log('File downloaded at: ' + result.file);
+        }
+      });
+    }
+  });
+  return true;
 };
